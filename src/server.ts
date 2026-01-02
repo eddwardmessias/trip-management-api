ab2b7717a03279245bcb8c88ed4ad3a206e1b1e7
eddwardmessias <eddward.messias@hotmail.com>
2026-01-01 22:33:59 -0300

    feat: add confirm-participants route

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import cors from '@fastify/cors';
 import { createTrip } from './routes/create-trip';
 import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod';
 import { confirmTrip } from './routes/confirm-trip';
+import { confirmTParticipants } from './routes/confirm-participant';
 
 const app = fastify()
 
@@ -15,6 +16,7 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
 app.register(confirmTrip);
+app.register(confirmTParticipants); 
 
 app.listen({port: 3333}).then(() => {
   console.log('Server is running!');
