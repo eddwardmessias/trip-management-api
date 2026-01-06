8841208b31394d9c38e23d370aaec2edb7f5bb1c
eddwardmessias <eddward.messias@hotmail.com>
2026-01-06 10:12:34 -0300

    feat: add routes update-trip, get-trip, get-participant

---
+++ src/routes/get-trip-details.ts
@@ -0,0 +1,43 @@
+import dayjs from 'dayjs';
+import { prisma } from '../lib/prisma';
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+
+
+export async function getTripDetails(app: FastifyInstance) {
+
+  app.withTypeProvider<ZodTypeProvider>().get('/trips/:tripId', {
+    schema: {
+      params: z.object({
+        tripId: z.string().uuid(),
+      })
+    }
+
+  }, 
+  async (request, reply) => {
+
+    const {tripId} = request.params;
+
+
+    const trip = await prisma.trip.findUnique({
+      select: {
+        id: true,
+        destination: true,
+        starts_at: true,
+        ends_at: true,
+        is_confirmed: true,
+      },
+      where: {
+        id: tripId,
+      }
+    })
+
+    if(!trip){
+      return new Error('Trip not found'  );
+    }
+
+    return {trip};
+  },
+  )
+}
